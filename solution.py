0e19b59e1f5f0bbb52a247a12bdbeeb325fa65c7
Spamium <hugo00pereira@gmail.com>
2020-11-10 16:18:34 +0000

    Add files via upload

--- solution.py
+++ solution.py
@@ -2,29 +2,32 @@ import search
 
 class PMDAProblem(search.class):
 	
+	# timeStep definition for algorithm tuning
+	# outside of project scope, good practice anyway
+	timeStep = 5
+	
 	# Lists of tuples
-	# TODO - ADD COMMENT WITH INDIVIDUAL LIST EXPLANATION
+	# Doctor list is composed of ( doctorID, efficiency )
 	doctorList = []
+	# Label list is composed of ( labelID, maximumWait, consultationTime )
 	labelList = []
+	# Patient list is composed of (patientID, currentWaitingTime, labelID, consultationTimeLeft, isBusy )
+	# isBusy is a boolean to help during cost calculations
 	patientList = []
 
 	
 	# state is a tuple!
-	# TODO - ADD COMMENT WITH STATE EXPLANATION
 	# state( cost, timeInstant, MD0, MD1, MD2, MD3, ..., MDn )
-	# MDn = ( MD , ID, patient, patient, patient, ... , lastPatient)
+	# MDn = ( MD , ID, patient, patient, ... , lastPatient)
 	# timeInstant - defined in pdf as 5min
 	state = ()
 	
-	
-	def __init__(self, initial, goal=None):
+	# our problem loads the initial data from a file
+	# initial state depends on how many doctors we have, it's not required or advised
+	def __init__(self, initial=None, goal=None):
 		
-		tempList = load(input("Select file to open:\n"))
-		
-		self.doctorList = tempList[0]
-		self.labelList = tempList[1]
-		self.patientList = tempList[2]
-	
+		load(input("Select file to open:\n"))
+			
 		#Initial state:
 		# (0, 0, MD0, ... , MDn)
 		#Doctors at state 0:
@@ -52,19 +55,30 @@ class PMDAProblem(search.class):
 		for
 		
 		return c + 1
+	
+	# this is the recursive function that digs for the answer
+	# state must include patient list information, we have to send it into the next step
+	def search(self, state, patientList):
 		
-	def search(self, state):
+		# we need to make sure we don't pollute this data
+		nextState = copy.deepcopy(state)
+		patientList = copy.deepcopy(self.patientList)
 		
-		nextState = []
+		# Rhis is our priority list for patients
+		# Right now we are strictly enforcing FIFO-like behaviour. 
+		# The longest waiting time goes first even if we have patient/doctor shuffling
 		patientHeuristic = []
-		
+		# we simply look through patients and add the tuple (patientID, totalWaitingTime) to our queue
+		# then quickly organize the priority list by decreasing waiting time
 		for p in self.patientList:
-			patientHeuristic.append([self.patientList[0], self.labelList[int(self.patientList[2])][1])
+			patientHeuristic.append((self.patientList[0], self.labelList[int(self.patientList[2])][1]))
 		patientHeuristic.sort(key=lambda x: x[1], reverse=true)
 		
+		# our new state can be defined as previous stateStep+1, where each step is 5min
+		# path cost can be defined as the cost of the target state. Initialized at 0 until state is defined
 		newState = [0, state[1] + 1]
 		for d in state[2:]:
-			for p in self.patientList:
+			for p in patientList:
 				if p[0] == patientHeuristic[0]:
 					newState.append(list(d).append(p[0])
 					p[3] -= 5*grabDoctor(self, d[0])[1]
@@ -84,7 +98,7 @@ class PMDAProblem(search.class):
 		return 0
 		
 		
-	def load(target):
+	def load(self, target):
 		f = open(target, "r")
 		doctorList = []
 		labelList = []
@@ -105,7 +119,11 @@ class PMDAProblem(search.class):
 				# and then we select the consultation time form that label - LabelList[n][2]
 				patientList.append( (prelist[1], prelist[2], prelist[3], labelList[int(prelist[3])][2], false) )
 		
-		return [doctorList, labelList, patientList]
+		self.doctorList = tempList[0]
+		self.labelList = tempList[1]
+		self.patientList = tempList[2]
+		
+		return
 		
 	def save(self):
 		#TODO - WRITE THIS!
